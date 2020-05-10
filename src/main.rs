cd8ef11a9e7d7e7b6665ccc0baab5b5989996f35
Noah <33094578+coolreader18@users.noreply.github.com>
2020-05-09 22:23:17 -0500

    Unify the different run commands

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use native_runner::{CommandRunner, TokioRunner};
 use std::ffi::OsStr;
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use tokio::process::Command;
 use tokio::{io, task};
 use wasi_runner::WasiProcess;
@@ -14,7 +14,7 @@ use wasmer_wasi::WasiVersion;
 use logic::RobotRunner;
 
 use anyhow::{anyhow, bail, Context};
-use clap::{App, AppSettings, Arg, ArgMatches, SubCommand};
+use clap::{App, AppSettings, Arg, SubCommand};
 use itertools::Itertools;
 use once_cell::sync::Lazy;
 
@@ -34,53 +34,33 @@ fn app() -> App<'static, 'static> {
         .version(clap::crate_version!())
         .author(clap::crate_authors!())
         .setting(AppSettings::SubcommandRequiredElseHelp)
-        .arg(
-            Arg::with_name("turns")
-                .short("t")
-                .long("turns")
-                .takes_value(true)
-                .value_name("TURN_NUM")
-                .default_value("10")
-                .global(true)
-                .help("Sets the number of turns to run")
-        )
         .subcommand(
             SubCommand::with_name("run")
                 .about("Run 2 robots against each other")
+                // TODO: polish the about string
                 .long_about(
-                    "Run 2 robots. If the robot identifier matches the regex /^[_\\w]+\\/[_\\w]+$/, e.g. 'user_1/robotv3_Final', \
+                    "Run 2 robots. If robot identifier matches the regex /^[_\\w]+\\/[_\\w]+$/, e.g. 'user_1/robotv3_Final', \
                     it will be interpreted as a robot published to https://robot-rumble.org; otherwise it will be interpreted as a path \
-                    to a local file that must be named with an extension of a supported language"
-                )
-                .arg(Arg::with_name("ROBOT1").required(true))
-                .arg(Arg::with_name("ROBOT2").required(true))
-        )
-        .subcommand(
-            SubCommand::with_name("run-command")
-                .about("Run 2 commands as robots")
-                .long_about(
-                    "Run 2 commands as robots. Each recieve a path to their source file as the first argument (after the ones provided \
+                    to a local file that must be named with an extension of a supported language. \
+                    command: or localrunner: : \n\
+                    Each recieve a path to their source file as the first argument (after the ones provided \
                     in the command string), and after they initalize, they should print a `Result<(), ProgramError>` in \
                     serde_json format and a newline. They will then start recieving newline-delimited `ProgramInput` json objects, and \
                     for each one should output a `ProgramOutput` json object followed by a newline. The match is over when stdin is closed, and \
                     the process may be forcefully terminated after that."
                 )
-                .arg(Arg::with_name("ROBOT1_EXE").required(true))
-                .arg(Arg::with_name("ROBOT1_SOURCE").required(true))
-                .arg(Arg::with_name("ROBOT2_EXE").required(true))
-                .arg(Arg::with_name("ROBOT2_SOURCE").required(true))
-        )
-        .subcommand(
-            SubCommand::with_name("wasm")
-                .about("Run 2 wasi modules as robots") 
-                .long_about(
-                    "Run a wasi module as a robot. This will be fully sandboxed, so is (probably) safe \
-                    to use with untrusted modules. The process will communicate the same way as described in the run-command about text."
+                .arg(Arg::with_name("ROBOT1").required(true))
+                .arg(Arg::with_name("ROBOT2").required(true))
+                .arg(
+                    Arg::with_name("turns")
+                        .short("t")
+                        .long("turns")
+                        .takes_value(true)
+                        .value_name("TURN_NUM")
+                        .default_value("10")
+                        .global(true)
+                        .help("Sets the number of turns to run")
                 )
-                .arg(Arg::with_name("ROBOT1_EXE").required(true))
-                .arg(Arg::with_name("ROBOT1_SOURCE").required(true))
-                .arg(Arg::with_name("ROBOT2_EXE").required(true))
-                .arg(Arg::with_name("ROBOT2_SOURCE").required(true))
         )
 }
 
@@ -120,6 +100,7 @@ impl Runner {
     async fn new_wasm(
         module: &WasmModule,
         version: WasiVersion,
+        args: &[String],
         dir: tempfile::TempDir,
     ) -> anyhow::Result<logic::ProgramResult<Self>> {
         let mut state = wasmer_wasi::state::WasiState::new("robot");
@@ -127,6 +108,7 @@ impl Runner {
         state
             .preopen(|p| p.directory(&dir).alias("source").read(true))
             .unwrap()
+            .args(args)
             .arg("/source/sourcecode");
         let imports =
             wasmer_wasi::generate_import_object_from_state(state.build().unwrap(), version);
@@ -141,8 +123,7 @@ impl Runner {
             .await
             .map(|r| Self::Wasi(r, dir)))
     }
-    async fn from_id(id: &OsStr) -> anyhow::Result<logic::ProgramResult<Runner>> {
-        let id = RobotId::from_osstr(id)?;
+    async fn from_id(id: &RobotId) -> anyhow::Result<logic::ProgramResult<Self>> {
         match id {
             RobotId::Published { user, robot } => {
                 let _ = (user, robot);
@@ -151,7 +132,26 @@ impl Runner {
             RobotId::Local { source, lang } => {
                 let sourcedir = make_sourcedir(source)?;
                 let (module, version) = lang.get_wasm();
-                Runner::new_wasm(module, version, sourcedir).await
+                Runner::new_wasm(module, version, &[], sourcedir).await
+            }
+            RobotId::Command { command, args } => {
+                let mut cmd = Command::new(command);
+                cmd.args(args);
+                Ok(TokioRunner::new_cmd(cmd).await.map(Self::Command))
+            }
+            RobotId::LocalRunner {
+                runner,
+                runner_args,
+                source,
+            } => {
+                let sourcedir = make_sourcedir(source)?;
+                let wasm = tokio::fs::read(runner)
+                    .await
+                    .with_context(|| format!("couldn't read {}", runner.display()))?;
+                let (module, version) = wasm_from_cache_or_compile(&wasm).with_context(|| {
+                    format!("couldn't compile wasm module at {}", runner.display())
+                })?;
+                Runner::new_wasm(&module, version, &runner_args, sourcedir).await
             }
         }
     }
@@ -160,65 +160,24 @@ impl Runner {
 async fn try_main() -> anyhow::Result<()> {
     let matches = app().get_matches();
     let nturns = clap::value_t!(matches.value_of("turns"), usize)?;
-    let (r1, r2) = get_runners(&matches).await?;
 
-    let output = logic::run(r1, r2, turn_cb, nturns).await;
-    println!("Output: {:?}", output);
+    if let Some(matches) = matches.subcommand_matches("run") {
+        let get_runner = |val_name| async move {
+            let id = RobotId::parse(matches.value_of_os(val_name).unwrap())
+                .with_context(|| format!("couldn't parse {}", val_name))?;
+            let runner = Runner::from_id(&id).await?;
+            Ok::<_, anyhow::Error>(runner)
+        };
+        let (r1, r2) = tokio::try_join!(get_runner("ROBOT1"), get_runner("ROBOT2"))?;
+        let output = logic::run(r1, r2, turn_cb, nturns).await;
+        println!("Output: {:?}", output);
+    }
 
     Ok(())
 }
 
-async fn get_runners(
-    matches: &ArgMatches<'static>,
-) -> anyhow::Result<(logic::ProgramResult<Runner>, logic::ProgramResult<Runner>)> {
-    let ret = if let Some(matches) = matches.subcommand_matches("run") {
-        tokio::try_join!(
-            Runner::from_id(matches.value_of_os("ROBOT1").unwrap()),
-            Runner::from_id(matches.value_of_os("ROBOT2").unwrap()),
-        )?
-    } else if let Some(matches) = matches.subcommand_matches("wasm") {
-        let make_runner = |exe_val, src_val| async move {
-            let sourcedir = make_sourcedir(matches.value_of_os(src_val).unwrap())?;
-
-            let wasm = fs::read(matches.value_of_os(exe_val).unwrap())
-                .context("couldn't read wasm source")?;
-            eprintln!("compiling wasm");
-            let module = wasmer_runtime::compile(&wasm).context("couldn't compile wasm module")?;
-            let version = wasmer_wasi::get_wasi_version(&module, false)
-                .unwrap_or(wasmer_wasi::WasiVersion::Latest);
-            eprintln!("done!");
-
-            Runner::new_wasm(&module, version, sourcedir).await
-        };
-        tokio::try_join!(
-            make_runner("ROBOT1_EXE", "ROBOT1_SOURCE"),
-            make_runner("ROBOT2_EXE", "ROBOT2_SOURCE"),
-        )?
-    } else if let Some(matches) = matches.subcommand_matches("run-command") {
-        let make_runner = |exe_val, src_val| async move {
-            let mut args = shell_words::split(matches.value_of(exe_val).unwrap())
-                .with_context(|| format!("Couldn't parse {} as shell arguments", exe_val))?
-                .into_iter();
-            let mut cmd = Command::new(args.next().ok_or_else(|| {
-                anyhow!("you must have at least one shell 'word' in {}", exe_val)
-            })?);
-            cmd.args(args);
-            cmd.arg(matches.value_of_os(src_val).unwrap());
-            Ok::<_, anyhow::Error>(TokioRunner::new_cmd(cmd).await.map(Runner::Command))
-        };
-
-        tokio::try_join!(
-            make_runner("ROBOT1_EXE", "ROBOT1_SOURCE"),
-            make_runner("ROBOT2_EXE", "ROBOT2_SOURCE"),
-        )?
-    } else {
-        unreachable!()
-    };
-    Ok(ret)
-}
-
 #[derive(Clone, Copy)]
-enum Lang {
+pub enum Lang {
     Python,
     Javascript,
 }
@@ -230,28 +189,36 @@ fn get_wasm_cache() -> Option<FileSystemCache> {
     unsafe { FileSystemCache::new(dir).ok() }
 }
 
+fn wasm_from_cache_or_compile(
+    wasm: &[u8],
+) -> wasmer_runtime::error::CompileResult<(WasmModule, WasiVersion)> {
+    let hash = WasmHash::generate(wasm);
+    let cache = get_wasm_cache();
+    let module = cache
+        .as_ref()
+        .and_then(|cache| cache.load(hash).ok())
+        .map_or_else(
+            || -> wasmer_runtime::error::CompileResult<_> {
+                let module = wasmer_runtime::compile(wasm)?;
+                if let Some(mut cache) = cache {
+                    cache.store(hash, module.clone()).ok();
+                }
+                Ok(module)
+            },
+            Ok,
+        )?;
+    let version = wasmer_wasi::get_wasi_version(&module, false).unwrap_or(WasiVersion::Latest);
+    Ok((module, version))
+}
+
 impl Lang {
     fn get_wasm(self) -> (&'static WasmModule, WasiVersion) {
         macro_rules! compiled_runner {
             ($name:literal) => {{
                 static MODULE: Lazy<(WasmModule, WasiVersion)> = Lazy::new(|| {
                     let wasm = include_bytes!(concat!("../../logic/webapp-dist/runners/", $name));
-                    let hash = WasmHash::generate(wasm);
-                    let cache = get_wasm_cache();
-                    let module = cache
-                        .as_ref()
-                        .and_then(|cache| cache.load(hash).ok())
-                        .unwrap_or_else(|| {
-                            let module = wasmer_runtime::compile(wasm)
-                                .expect(concat!("couldn't compile wasm module ", $name));
-                            if let Some(mut cache) = cache {
-                                cache.store(hash, module.clone()).ok();
-                            }
-                            module
-                        });
-                    let version =
-                        wasmer_wasi::get_wasi_version(&module, false).expect("module isn't wasi");
-                    (module, version)
+                    wasm_from_cache_or_compile(wasm)
+                        .expect(concat!("couldn't compile wasm module ", $name))
                 });
                 let (module, version) = &*MODULE;
                 (module, *version)
@@ -264,23 +231,85 @@ impl Lang {
     }
 }
 
-enum RobotId<'a> {
-    Published { user: &'a str, robot: &'a str },
-    Local { source: &'a Path, lang: Lang },
+pub enum RobotId {
+    Published {
+        user: String,
+        robot: String,
+    },
+    Local {
+        source: PathBuf,
+        lang: Lang,
+    },
+    Command {
+        command: String,
+        args: Vec<String>,
+    },
+    LocalRunner {
+        runner: PathBuf,
+        runner_args: Vec<String>,
+        source: PathBuf,
+    },
 }
 
-impl<'a> RobotId<'a> {
-    fn from_osstr(s: &'a OsStr) -> anyhow::Result<Self> {
-        if let Some(s) = s.to_str() {
-            if let Some((user, robot)) = s.split('/').collect_tuple() {
-                let valid_ident =
-                    |s: &str| s.chars().all(|c| c.is_ascii_alphanumeric() || c == '_');
-                if valid_ident(user) && valid_ident(robot) {
-                    return Ok(RobotId::Published { user, robot });
+impl RobotId {
+    fn parse(s: &OsStr) -> anyhow::Result<Self> {
+        let s = match s.to_str() {
+            Some(s) => s,
+            None => return Self::from_path(PathBuf::from(s)),
+        };
+        let parse_command = |s| -> anyhow::Result<_> {
+            let mut args = shell_words::split(s)
+                .context("Couldn't parse as shell arguments")?
+                .into_iter();
+            let command = args.next().ok_or_else(|| {
+                anyhow!("you must have at least one shell 'word' in the command string")
+            })?;
+            Ok((command, args.collect_vec()))
+        };
+        if let Some((typ, content)) = s.splitn(2, ':').collect_tuple() {
+            match typ {
+                "file" | "local" => Self::from_path(PathBuf::from(content)),
+                "published" => Self::from_published(content).ok_or_else(|| {
+                    anyhow!(
+                        "invalid published robot id {:?}; it must be in the form of `user/robot` with only \
+                        alphanumeric characters and underscores",
+                        content
+                    )
+                }),
+                "command" => {
+                    let (command, args) = parse_command(content)?;
+                    Ok(Self::Command { command, args })
                 }
+                "localrunner" => {
+                    let (runner, mut runner_args) = parse_command(content)?;
+                    let runner = PathBuf::from(runner);
+                    let source = PathBuf::from(runner_args.pop().ok_or_else(|| {
+                        anyhow!("you must have a source argument to the local runner")
+                    })?);
+                    Ok(Self::LocalRunner { runner, runner_args, source })
+                }
+                _ => bail!("unknown runner type {:?}", typ)
             }
+        } else if let Some(published) = Self::from_published(s) {
+            Ok(published)
+        } else {
+            Self::from_path(PathBuf::from(s))
         }
-        let source = Path::new(s);
+    }
+    fn from_published(s: &str) -> Option<Self> {
+        s.split('/').collect_tuple().and_then(|(user, robot)| {
+            let valid_ident = |s: &str| s.chars().all(|c| c.is_ascii_alphanumeric() || c == '_');
+            if valid_ident(user) && valid_ident(robot) {
+                Some(Self::Published {
+                    user: user.to_owned(),
+                    robot: robot.to_owned(),
+                })
+            } else {
+                None
+            }
+        })
+    }
+    fn from_path(source: PathBuf) -> anyhow::Result<Self> {
         let ext = source.extension().ok_or_else(|| {
             anyhow!("your robot file must have an extension so that we know what language it's in")
         })?;
