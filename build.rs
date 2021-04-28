7c935e2bbff54611a4ae066c692a9ae9ea292927
Noah <33094578+coolreader18@users.noreply.github.com>
2021-04-27 22:13:47 -0500

    Compile windows runners in linux

--- build.rs
+++ build.rs
@@ -7,64 +7,111 @@ use wasmer_compiler_cranelift::Cranelift as Compiler;
 #[cfg(feature = "build-llvm")]
 use wasmer_compiler_llvm::LLVM as Compiler;
 
-use wasmer_engine::Artifact;
+enum CompilationSource {
+    Precompiled(PathBuf),
+    #[cfg(any(feature = "build-cranelift", feature = "build-llvm"))]
+    Compiler {
+        engine: wasmer::JITEngine,
+        runners_dir: PathBuf,
+        jit_ext: &'static str,
+        tunables: wasmer::BaseTunables,
+    },
+}
 
 fn main() {
-    let mut features = wasmer::CpuFeature::set();
-    for feat in env::var("CARGO_CFG_TARGET_FEATURE").unwrap().split(',') {
-        if let Ok(feat) = feat.parse() {
-            features.insert(feat);
+    let source = match env::var_os("COMPILED_RUNNERS") {
+        Some(dir) => CompilationSource::Precompiled(dir.into()),
+        #[cfg(not(any(feature = "build-cranelift", feature = "build-llvm")))]
+        None => {
+            panic!("need build-cranelift or build-llvm or the COMPILED_RUNNERS env var")
         }
-    }
-    let target = wasmer::Target::new(env::var("TARGET").unwrap().parse().unwrap(), features);
-    let tunables = wasmer::BaseTunables::for_target(&target);
-    let jit_ext = wasmer::JITArtifact::get_default_extension(target.triple());
-    let engine = wasmer::JIT::new(Compiler::new()).target(target).engine();
+        #[cfg(any(feature = "build-cranelift", feature = "build-llvm"))]
+        None => {
+            let mut features = wasmer::CpuFeature::set();
+            for feat in env::var("CARGO_CFG_TARGET_FEATURE").unwrap().split(',') {
+                if let Ok(feat) = feat.parse() {
+                    features.insert(feat);
+                }
+            }
+            let target =
+                wasmer::Target::new(env::var("TARGET").unwrap().parse().unwrap(), features);
+            let tunables = wasmer::BaseTunables::for_target(&target);
+            let jit_ext = wasmer::JITArtifact::get_default_extension(target.triple());
+            let engine = wasmer::JIT::new(Compiler::new()).target(target).engine();
+
+            let runners_dir = fs::canonicalize("../logic/wasm-dist/lang-runners")
+                .expect("need to run logic/build-wasm.sh");
+
+            CompilationSource::Compiler {
+                engine,
+                runners_dir,
+                jit_ext,
+                tunables,
+            }
+        }
+    };
 
     let lang_runners = [("Python", "pyrunner"), ("Javascript", "jsrunner")];
 
-    let runners_dir = fs::canonicalize("../logic/wasm-dist/lang-runners")
-        .expect("need to run logic/build-wasm.sh");
     let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
 
     let mut match_lang = fs::File::create(out_dir.join("lang_runners.rs")).unwrap();
     writeln!(match_lang, "match lang {{").unwrap();
 
     for (lang, runner) in &lang_runners {
-        let mut src = runners_dir.join(runner);
-        src.set_extension("wasm");
-        let mut dst = out_dir.join(runner);
-        dst.set_extension(jit_ext);
+        let (path, include_bin) = match &source {
+            CompilationSource::Precompiled(dir) => {
+                let mut wjit = dir.join(runner);
+                wjit.set_extension("wjit");
+                (wjit, true)
+            }
+            #[cfg(any(feature = "build-cranelift", feature = "build-llvm"))]
+            CompilationSource::Compiler {
+                engine,
+                runners_dir,
+                jit_ext,
+                tunables,
+            } => {
+                use wasmer_engine::Artifact;
 
-        println!("compiling {}", runner);
+                let mut src = runners_dir.join(runner);
+                src.set_extension("wasm");
+                let mut dst = out_dir.join(runner);
+                dst.set_extension(*jit_ext);
 
-        println!("cargo:rerun-if-changed={}", src.display());
+                println!("compiling {}", runner);
 
-        let needs_updating = src
-            .metadata()
-            .and_then(|m| Ok((m, dst.metadata()?)))
-            .and_then(|(src, dst)| Ok(src.modified()? > dst.modified()?))
-            .unwrap_or(true);
+                println!("cargo:rerun-if-changed={}", src.display());
 
-        if needs_updating {
-            let wasm_source = fs::read(&src).unwrap();
-            let artifact = wasmer::JITArtifact::new(&engine, &wasm_source, &tunables).unwrap();
+                let needs_updating = src
+                    .metadata()
+                    .and_then(|m| Ok((m, dst.metadata()?)))
+                    .and_then(|(src, dst)| Ok(src.modified()? > dst.modified()?))
+                    .unwrap_or(true);
 
-            fs::write(&dst, artifact.serialize().unwrap()).unwrap();
-        }
+                if needs_updating {
+                    let wasm_source = fs::read(&src).unwrap();
+                    let artifact =
+                        wasmer::JITArtifact::new(engine, &wasm_source, tunables).unwrap();
+
+                    fs::write(&dst, artifact.serialize().unwrap()).unwrap();
+                }
+
+                (dst, cfg!(feature = "build-llvm"))
+            }
+        };
 
         writeln!(
             match_lang,
             "    Lang::{} => lang_runner!({}({:?}){}),",
             lang,
-            //
-            if cfg!(feature = "build-llvm") {
+            if include_bin {
                 "include_bytes!"
             } else {
                 "&std::fs::read"
             },
-            dst,
-            if cfg!(feature = "build-llvm") {
+            path,
+            if include_bin {
                 ""
             } else {
                 r#".expect("should compile with --features=build-llvm when distributing")"#
