db060009c45d5a1306b196d4aaad3a2becb1cfff
Anton <antonoutkine@gmail.com>
2021-04-29 10:56:39 -0500

    Fix turn counter display

--- src/display.rs
+++ src/display.rs
@@ -8,7 +8,7 @@ pub fn display_turn(turn: &CallbackInput) -> io::Result<()> {
     let mut bold = ColorSpec::new();
     bold.set_bold(true);
     out.set_color(&bold)?;
-    writeln!(out, "After turn {}:", turn.state.turn)?;
+    writeln!(out, "After turn {}:", turn.state.turn + 1)?;
     out.reset()?;
 
     let grid_map = GridMap::from(&turn.state.objs);
